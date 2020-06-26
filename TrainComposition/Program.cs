d30154db70a4b2656ed5f1dbd3ec00c8d3cad5da
Nicolas Mendez <nicomenfede@gmail.com>
2020-06-26 14:43:21 +0100

    I solve it using LinkList

---
+++ TrainComposition/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TrainComposition
+{
+    LinkedList<int> linkedLists = new LinkedList<int>();
+ 
+    public void AttachWagonFromLeft(int wagonId)
+    {
+        linkedLists.AddFirst(wagonId);
+    }
+
+    public void AttachWagonFromRight(int wagonId)
+    {
+        linkedLists.AddLast(wagonId);
+    }
+
+    public int DetachWagonFromLeft()
+    {
+        var result = linkedLists.First.Value;
+        linkedLists.RemoveFirst();
+        return result;
+    }
+
+    public int DetachWagonFromRight()
+    {
+        var result = linkedLists.Last.Value;
+        linkedLists.RemoveLast();
+        return result;
+    }
+
+    public static void Main(string[] args)
+    {
+        TrainComposition train = new TrainComposition();
+        train.AttachWagonFromLeft(7);
+        train.AttachWagonFromLeft(13);
+        Console.WriteLine(train.DetachWagonFromRight()); // 7 
+        Console.WriteLine(train.DetachWagonFromLeft()); // 13
+    }
+}
